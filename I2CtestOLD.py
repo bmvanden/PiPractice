83474d0cd3e4deb55189fb1999f65ad0a060cb98
bmvanden <bmvanden@ualberta.ca>
2019-06-11 02:11:17 -0600

    Fixed I2C Block read

--- I2CtestOLD.py
+++ I2CtestOLD.py
@@ -3,12 +3,15 @@ I2Ctest.py
 
 Tests I2C communication between Pi and ATMega
 """
-
-from smbus2 import SMBusWrapper
+import struct
+import smbus2
+bus = smbus2.SMBus(1)
 import time
 
-ATMegaData = [0, 1, 3, 50, 80, 0, 0, 0]
+ATMegaData = [0, 1, 3, 50] #, 80, 0, 0, 0])
 PiData = [3, 10, 120]
+
+time.sleep(2)
 
 while (1):
 	"""
@@ -17,10 +20,25 @@ while (1):
 
 	Address: 	8
 	Offset:  	0
-	# of Bytes:	7
+	of Bytes:	7
 	"""
-	with SMBusWrapper(1) as bus:
-		ATMegaData = bus.read_i2c_block_data(8, 0, 7)
+	with smbus2.SMBusWrapper(1) as bus:
+		ATMegaData = bus.read_i2c_block_data(8, 0, 4)
+
+#	data = bus.read_byte_data(8, 0)
+
+#	block = bus.read_i2c_block_data(8, 0, 4)
+#	block2 = struct.unpack("<HHHH",block)
+#	print(data)
+#	print('\n')
+
+#	for received in block:
+#		ATMegaData[1] = received
+
+#	ATMegaData = struct.unpack('l', ''.join([chr(i) for i in block[:4]))[0]
+#	ATMegaData[0] = bus.read_byte(8)
+#	ATMegaData[1] = bus.read_byte(8,1)
+
 
 	time.sleep(0.5)
 
@@ -32,7 +50,7 @@ while (1):
 	Offset:		0
 	# of bytes: 3
 	"""
-	with SMBusWrapper(1) as bus:
+	with smbus2.SMBusWrapper(1) as bus:
 		bus.write_i2c_block_data(8, 0, PiData)
 
 	time.sleep(0.5)
@@ -40,7 +58,10 @@ while (1):
 	print("ATMega Data: ")
 	for i in ATMegaData:
 		print(i)
+	print(ATMegaData)
 
 	print("\nPi Data: ")
 	for i in PiData:
 		print(i)
+
+#	time.sleep(1)
